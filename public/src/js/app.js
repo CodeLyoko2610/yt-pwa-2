ce26dbc376ba78e0c1ff7458e6fa7861d3e8c9f5
chuongcuu <dangchuong1999@outlook.com>
2020-02-19 15:46:00 +0200

    app.js: Register the serviceWorker | sw.js: Add event listeners for the serviceWorker.

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -3,6 +3,15 @@ var pwaCardContent = pwaCard.querySelector('.card__content');
 var pwaCardDetails = pwaCard.querySelector('.card__details');
 var detailsShown = false;
 
+//Register a service worker
+//Navigator is the browser
+//Check if the browser supports service worker prior to installation
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').then(function () {
+    console.log('SW registered.');
+  }); //it starts from the "start" folder of the application -[TEST LATER] specified in manifest.json 
+}
+
 pwaCard.addEventListener('click', function (event) {
   if (!detailsShown) {
     detailsShown = true;
