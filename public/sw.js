c51aaab50ce42348f4239bc39673d2e75906c19f
chuongcuu <dangchuong1999@outlook.com>
2020-02-19 16:09:31 +0200

    sw.js: create a cache-storage and cache data.

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,19 @@
-self.addEventListener('install', () => {
-    console.log('SW installed.');
-})
+self.addEventListener('install', function(event) {
+  console.log('SW installed.');
+  event.waitUntil(
+    caches.open('static-cache').then(function(cache) {
+      cache.addAll([
+        './',
+        './index.html',
+        './src/js/app.js',
+        './src/css/app.css',
+        './src/images/pwa.jpg',
+        'https://fonts.googleapis.com/css?family=Raleway:400,700'
+      ]);
+    })
+  );
+});
 
 self.addEventListener('activate', () => {
-    console.log('SW activated.');
-})
+  console.log('SW activated.');
+});
